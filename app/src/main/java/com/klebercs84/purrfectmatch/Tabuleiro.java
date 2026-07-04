9dd3c65c01458d140d3d370b1b57ae9ea9623a8f
Kleber Souza <klebercaitanosouza@gmail.com>
2026-07-04 14:38:09 -0300

    feat: cria classes Gato, Tabuleiro e GameView com tabuleiro visual

--- app/src/main/java/com/klebercs84/purrfectmatch/Tabuleiro.java
+++ app/src/main/java/com/klebercs84/purrfectmatch/Tabuleiro.java
@@ -8,11 +8,21 @@ public class Tabuleiro {
     private Gato[][] grade; // matriz de objetos Gato
     private Random random;
 
-    // Construtor - cri i tabuleiro com gatos aleatórios
+    // Construtor - cria o tabuleiro com gatos aleatórios
     public Tabuleiro(){
         random = new Random();
         grade = new Gato [TAMANHO][TAMANHO];
         inicializar();
+    }
+
+    // Preenche cada célula com um Gato de tipo aleatório
+    private void inicializar(){
+        for (int linha = 0;linha<TAMANHO;linha++){
+            for (int coluna = 0; coluna < TAMANHO;coluna ++){
+                int tipoAleatorio = random.nextInt(Gato.TOTAL_TIPOS);
+                grade[linha][coluna] = new Gato(tipoAleatorio);
+            }
+        }
     }
 
     // Retorna o gato numa posição específica
